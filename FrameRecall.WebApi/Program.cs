e1788c8c45b3d1aa7ed61f3662ca25a4a4685f85
butCHERrRy <borzov_ga@mail.ru>
2026-09-22 15:03:46 +0400

    коммит форка, апишка от вебапи до сервиса

--- FrameRecall.WebApi/Program.cs
+++ FrameRecall.WebApi/Program.cs
@@ -7,6 +7,16 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 // Добавление сервисов
 builder.Services.AddOpenApi();
 
+builder.Services.AddSingleton<IMongoClient>(
+    _ => new MongoClient(builder.Configuration.GetConnectionString("MongoDb")));
+
+builder.Services.AddScoped<IMongoDatabase>(
+    sp => sp.GetRequiredService<IMongoClient>().GetDatabase("framerecall"));
+
+builder.Services.AddScoped<IRepository<DataFilm>, FilmRepository>();
+
+builder.Services.AddScoped<IFilmService, FilmService>();
+
 WebApplication app = builder.Build();
 
 // Настройка middleware для development окружения
@@ -18,3 +28,5 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.Run();
+
+
